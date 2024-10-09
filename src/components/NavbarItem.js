db8ccb74b191ba627f5230cc0937802f76d4358e
AbdumalikovIkboljon <abdumalikovikboljon0102@gmail.com>
2024-10-09 21:06:09 +0900

    create navbar component

---
+++ src/components/NavbarItem.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+import { useSearchParams } from 'next/navigation'
+
+const NavbarItem = ( { title, param }) => {
+
+    const searchParams = useSearchParams()
+    const genre = searchParams.get('genre')
+
+  return (
+    <div>
+        <Link className={`hover:text-amber-400 ${genre && genre === param && 'underline underline-offset-8 decoration-4 decoration-amber-400'}`} href={`/?genre=${param}`}>{title}</Link>
+    </div>
+  )
+}
+
+export default NavbarItem
