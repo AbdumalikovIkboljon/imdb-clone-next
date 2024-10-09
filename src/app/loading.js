932bc9d58935c834e9cde238030f04f7b1eb78a7
AbdumalikovIkboljon <abdumalikovikboljon0102@gmail.com>
2024-10-09 22:33:17 +0900

    add loader

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+        <img src="loading.svg" alt="loader" />
+    </div>
+  )
+}
+
+export default loading
